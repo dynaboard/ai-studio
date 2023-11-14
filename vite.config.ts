09238d7a0d3fc63e6f72b3c29cb3542568d3dd17
Omar Estrella <omar@dynaboard.com>
2023-11-13 20:06:58 -0500

    Electron now

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,87 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import react from '@vitejs/plugin-react'
+import { rmSync } from 'node:fs'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
+
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  rmSync('dist-electron', { recursive: true, force: true })
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  const isServe = command === 'serve'
+  const isBuild = command === 'build'
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
+
+  return {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src'),
+      },
     },
-  },
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 4000,
-    strictPort: true,
-  },
-  envPrefix: [
-    'VITE_',
-    'TAURI_PLATFORM',
-    'TAURI_ARCH',
-    'TAURI_FAMILY',
-    'TAURI_PLATFORM_VERSION',
-    'TAURI_PLATFORM_TYPE',
-    'TAURI_DEBUG',
-  ],
-  build: {
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
-    sourcemap: !!process.env.TAURI_DEBUG,
-    minify: false,
-  },
-}));
+    plugins: [
+      react(),
+      electron([
+        {
+          // Main-Process entry file of the Electron App.
+          entry: 'electron/main/index.ts',
+          onstart(options) {
+            if (process.env.VSCODE_DEBUG) {
+              console.log(
+                /* For `.vscode/.debug.script.mjs` */ '[startup] Electron App',
+              )
+            } else {
+              options.startup()
+            }
+          },
+          vite: {
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: 'dist-electron/main',
+              rollupOptions: {
+                external: Object.keys(
+                  'dependencies' in pkg ? pkg.dependencies : {},
+                ),
+              },
+            },
+          },
+        },
+        {
+          entry: 'electron/preload/index.ts',
+          onstart(options) {
+            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+            // instead of restarting the entire Electron App.
+            options.reload()
+          },
+          vite: {
+            build: {
+              sourcemap: sourcemap ? 'inline' : undefined, // #332
+              minify: isBuild,
+              outDir: 'dist-electron/preload',
+              rollupOptions: {
+                external: Object.keys(
+                  'dependencies' in pkg ? pkg.dependencies : {},
+                ),
+              },
+            },
+          },
+        },
+      ]),
+      // Use Node.js API in the Renderer-process
+      renderer(),
+    ],
+    server:
+      process.env.VSCODE_DEBUG &&
+      (() => {
+        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
+        return {
+          host: url.hostname,
+          port: +url.port,
+        }
+      })(),
+    clearScreen: false,
+  }
+})
