33d5742fbf8f0026486a0ee74ed47458d448c411
Cyrus Goh <hello@lovincyrus.com>
2023-11-15 15:31:42 -0800

    rework models to remove or download

--- src/components/downloads/model-downloader.tsx
+++ src/components/downloads/model-downloader.tsx
@@ -13,9 +13,12 @@ import {
 } from '@/components/ui/alert-dialog'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { useModelManager } from '@/providers/models/provider'
+import { Button } from '../ui/button'
+import { Download, LucideTrash } from 'lucide-react'
+import { Label } from '../ui/label'
 
 export function ModelDownloader() {
-  const [showExistingFileDialog, setShowExistingFileDialog] = useState(false)
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false)
 
   const modelManager = useModelManager()
 
@@ -27,23 +30,6 @@ export function ModelDownloader() {
 
   return (
     <div className="grid h-full min-h-0 w-full grid-cols-1">
-      <AlertDialog open={showExistingFileDialog}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Model Downloaded</AlertDialogTitle>
-            <AlertDialogDescription>
-              You&rsquo;ve already downloaded this model. If you need to
-              redownload for any reason, please delete the existing model file
-              and try again.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel onClick={() => setShowExistingFileDialog(false)}>
-              Ok
-            </AlertDialogCancel>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
 
       <ScrollArea className="grid h-full w-full gap-4 overflow-auto">
         <div className="mx-auto mb-4 flex flex-1 flex-col gap-4">
@@ -53,12 +39,18 @@ export function ModelDownloader() {
                 key={model.name}
                 className="grid grid-rows-[min-content,_min-content,_80px] gap-1"
               >
-                <span>{model.name}</span>
+                <Label>{model.name}</Label>
                 <p className="mb-2 text-xs leading-normal text-muted-foreground">
                   {model.description}
                 </p>
                 <div className="flex gap-2">
                   {model.files.map((file) => {
+                    const hasLocalFile = availableModels.some((model) =>
+                      model.files.some(
+                        (localFile) => localFile.name === file.name,
+                      ),
+                    )
+
                     return (
                       <a
                         href={file.url}
@@ -66,26 +58,91 @@ export function ModelDownloader() {
                         className="flex flex-1 flex-col rounded-md border bg-secondary px-2 py-1"
                         download={file.name}
                         onClickCapture={(event) => {
-                          const hasLocalFile = availableModels.some((model) =>
-                            model.files.some(
-                              (localFile) => localFile.name === file.name,
-                            ),
-                          )
                           if (hasLocalFile) {
                             event.preventDefault()
                             event.stopPropagation()
 
-                            setShowExistingFileDialog(true)
+                            setShowDeleteDialog(true)
                           }
                         }}
                       >
-                        <span className="text-xs">Faster, less accurate</span>
-                        <span className="text-xs">
-                          Quantization: {file.quantization}
-                        </span>
-                        <span className="text-xs">
-                          Size: {prettyBytes(file.sizeBytes)}
-                        </span>
+                        <div className="grid grid-cols-[1fr_36px]">
+                          <div className="flex flex-col gap-2">
+                            <span className="text-xs">
+                              Faster, less accurate
+                            </span>
+                            <span className="text-xs">
+                              Quantization: {file.quantization}
+                            </span>
+                            <span className="text-xs">
+                              Size: {prettyBytes(file.sizeBytes)}
+                            </span>
+                          </div>
+                          <div className="flex flex-col justify-center items-center">
+                            {hasLocalFile ? (
+                              <>
+                                <Button
+                                  size="sm"
+                                  variant="iconButton"
+                                  className="p-0 hover:text-destructive"
+                                  onClick={() => setShowDeleteDialog(true)}
+                                >
+                                  <LucideTrash size={16} />
+                                </Button>
+                                <AlertDialog
+                                  open={showDeleteDialog}
+                                  onOpenChange={setShowDeleteDialog}
+                                >
+                                  <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                      <AlertDialogTitle>
+                                        Are you sure absolutely sure?
+                                      </AlertDialogTitle>
+                                      <AlertDialogDescription>
+                                        This action cannot be undone. This model
+                                        will no longer be accessible by you.
+                                      </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                      <AlertDialogCancel
+                                        onClick={() => {
+                                          setShowDeleteDialog(false)
+                                        }}
+                                      >
+                                        Cancel
+                                      </AlertDialogCancel>
+                                      <Button
+                                        variant="destructive"
+                                        onClick={() => {
+                                          modelManager.deleteModelFile(
+                                            file.name,
+                                          )
+                                          setShowDeleteDialog(false)
+
+                                          // TODO: toast
+                                          // toast({
+                                          //   description:
+                                          //     'This model has been deleted.',
+                                          // })
+                                        }}
+                                      >
+                                        Delete
+                                      </Button>
+                                    </AlertDialogFooter>
+                                  </AlertDialogContent>
+                                </AlertDialog>
+                              </>
+                            ) : (
+                              <Button
+                                size="sm"
+                                variant="iconButton"
+                                className="p-0 hover:text-destructive"
+                              >
+                                <Download size={16} />
+                              </Button>
+                            )}
+                          </div>
+                        </div>
                       </a>
                     )
                   })}
--- src/routes/Models.tsx
+++ src/routes/Models.tsx
@@ -36,101 +36,11 @@ export function ModelsPage() {
   return (
     <div
       className={cn(
-        'flex h-full w-full',
+        'flex h-full w-full p-4',
         availableModels.length > 0 ? 'flex-col' : null,
       )}
     >
-      {availableModels.length > 0 ? (
-        <div className="flex flex-col gap-2 p-2">
-          {availableModels.map((model) => {
-            return (
-              <div key={model.name} className="flex flex-col gap-1">
-                <p className="text-lg">{model.name}</p>
-                <div className="grid grid-cols-2 gap-2">
-                  {model.files.map((file) => {
-                    return (
-                      <div
-                        key={file.name}
-                        className="flex justify-between gap-1 rounded-md border bg-secondary p-2"
-                      >
-                        <div className="flex flex-col gap-1">
-                          <span className="text-sm">{file.name}</span>
-                          <span className="text-sm">
-                            Quantization: {file.quantization}
-                          </span>
-                        </div>
-
-                        <div>
-                          <Button
-                            size="sm"
-                            variant="iconButton"
-                            className="p-0 hover:text-destructive"
-                            onClick={() => setShowDeleteDialog(true)}
-                          >
-                            <LucideTrash size={16} />
-                          </Button>
-                          <AlertDialog
-                            open={showDeleteDialog}
-                            onOpenChange={setShowDeleteDialog}
-                          >
-                            <AlertDialogContent>
-                              <AlertDialogHeader>
-                                <AlertDialogTitle>
-                                  Are you sure absolutely sure?
-                                </AlertDialogTitle>
-                                <AlertDialogDescription>
-                                  This action cannot be undone. This model will
-                                  no longer be accessible by you.
-                                </AlertDialogDescription>
-                              </AlertDialogHeader>
-                              <AlertDialogFooter>
-                                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                <Button
-                                  variant="destructive"
-                                  onClick={() => {
-                                    modelManager.deleteModelFile(file.name)
-                                    setShowDeleteDialog(false)
-                                    // toast({
-                                    //   description:
-                                    //     'This model has been deleted.',
-                                    // })
-                                  }}
-                                >
-                                  Delete
-                                </Button>
-                              </AlertDialogFooter>
-                            </AlertDialogContent>
-                          </AlertDialog>
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            )
-          })}
-        </div>
-      ) : null}
-      <div className="flex h-full w-full p-2">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button>
-              <LucidePlusCircle size={16} />
-              <span className="pl-4">Add Model</span>
-            </Button>
-          </SheetTrigger>
-
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>Download Model</SheetTitle>
-            </SheetHeader>
-
-            <div className="h-full w-full">
-              <ModelDownloader />
-            </div>
-          </SheetContent>
-        </Sheet>
-      </div>
+      <ModelDownloader />
     </div>
   )
 }
