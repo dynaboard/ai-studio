d0390b53cafa74c48d77e99c94deb8d2b90692f8
Cyrus Goh <hello@lovincyrus.com>
2023-11-16 20:42:03 -0800

    move clipboard to bottom

--- src/components/codeblock.tsx
+++ src/components/codeblock.tsx
@@ -1,11 +1,9 @@
-import { Check, Copy } from 'lucide-react'
+import { Check, Clipboard } from 'lucide-react'
 import { FC, memo } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 import { useCopyToClipboard } from '@/lib/hooks/use-copy-to-clipboard'
-
-import { Button } from './ui/button'
 
 interface Props {
   language: string
@@ -22,39 +20,28 @@ export const CodeBlock: FC<Props> = memo(({ language, value }) => {
 
   return (
     <div className="codeblock relative my-3 w-full font-sans text-xs">
-      {/* rgb(40, 44, 52) is from the default SyntaxHighlighter's bg */}
-      <div className="relative flex h-10 items-center justify-between rounded-t-md bg-zinc-800 px-4 py-2 font-sans text-xs text-zinc-100">
-        <span className="text-xs lowercase">{language}</span>
-        <div className="flex items-center">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="text-xs hover:bg-inherit hover:text-muted-foreground focus-visible:ring-1 focus-visible:ring-slate-700 focus-visible:ring-offset-0"
-            onClick={onCopy}
-          >
-            {isCopied ? (
-              <Check className="h-4 w-4" />
-            ) : (
-              <Copy className="h-4 w-4" />
-            )}
-            <span className="sr-only">Copy code</span>
-          </Button>
-        </div>
-      </div>
-
       <SyntaxHighlighter
         language={language}
         style={oneDark}
         PreTag="div"
-        customStyle={{
-          margin: 0,
-          borderRadius:
-            // Essentially `.rounded-b-md`
-            '0 0 calc(var(--radius) - 2px) calc(var(--radius) - 2px)',
-        }}
+        customStyle={{ margin: 0 }}
       >
         {value}
       </SyntaxHighlighter>
+
+      <div className="relative flex items-center justify-between">
+        <div className="mt-2 flex items-center">
+          {isCopied ? (
+            <Check className="h-3 w-3 text-muted-foreground/80" />
+          ) : (
+            <Clipboard
+              className="h-3 w-3 cursor-pointer text-muted-foreground/80"
+              onClick={onCopy}
+            />
+          )}
+          <span className="sr-only">Copy code</span>
+        </div>
+      </div>
     </div>
   )
 })
