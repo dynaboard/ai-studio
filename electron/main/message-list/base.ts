420e024cb1dcec0db8cd3bbf8bfcec0b52b0b975
Ny Saechao <nysaechao@outlook.com>
2023-11-20 12:04:32 -0800

    Add basic system prompt and chat memory so chats can be conversational (#14)
    Update rebase

---
+++ electron/main/message-list/base.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type MessageListInput = {
+  role: 'user' | 'assistant'
+  // TODO: Currently store messages as string, but maybe use rich text implementation
+  // text, entities: { offset, length, value } or {{ }} impl could work as well
+  message: string
+}
+
+export abstract class BaseMessageList {
+  abstract format(): string
+  abstract add(value: MessageListInput): void
+  abstract clear(): void
+}
