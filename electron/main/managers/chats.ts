7db9055e10ca79d2c32a7c0578903f4d38ab293c
Cyrus Goh <hello@lovincyrus.com>
2023-11-21 13:22:06 -0800

    colocate const for now

--- electron/main/managers/chats.ts
+++ electron/main/managers/chats.ts
@@ -19,6 +19,17 @@ import {
 
 const SYSTEM_PROMPT = `You are a helpful AI assistant.`
 
+const defaultSystemPrompt = `You are a helpful AI assistant that remembers previous conversation between yourself the "assistant" and a human the "user":
+### user:
+<previous user message>
+### assistant:
+<previous AI assistant message>
+
+### user:
+<new user prompt>
+
+The AI's task is to understand the context and utilize the previous conversation in addressing the user's questions or requests.`
+
 export type ChatSession = {
   modelName: string
   session: LlamaChatSession
--- src/components/configuration.tsx
+++ src/components/configuration.tsx
@@ -11,11 +11,21 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
-import { defaultSystemPrompt } from '@/lib/constants'
 
 import { MaxTokensSelector } from './parameters/max-token'
 import { TemperatureSelector } from './parameters/temperature'
 import { TopPSelector } from './parameters/top-p'
+
+const defaultSystemPrompt = `You are a helpful AI assistant that remembers previous conversation between yourself the "assistant" and a human the "user":
+### user:
+<previous user message>
+### assistant:
+<previous AI assistant message>
+
+### user:
+<new user prompt>
+
+The AI's task is to understand the context and utilize the previous conversation in addressing the user's questions or requests.`
 
 function Configuration() {
   return (
