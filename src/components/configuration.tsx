a05fc54a0b14a3c1dbbe883dc68be113a47cf66e
Cyrus Goh <hello@lovincyrus.com>
2023-11-21 15:06:38 -0800

    clean up

--- src/components/configuration.tsx
+++ src/components/configuration.tsx
@@ -1,11 +1,7 @@
 import { LucideSettings } from 'lucide-react'
 import React from 'react'
-import Textarea from 'react-textarea-autosize'
-import { Drawer } from 'vaul'
 
 import { Button } from '@/components/ui/button'
-import { DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
-import { Label } from '@/components/ui/label'
 import {
   Popover,
   PopoverContent,
@@ -13,23 +9,13 @@ import {
 } from '@/components/ui/popover'
 
 import { MaxTokensSelector } from './parameters/max-token'
+import { SystemPrompt } from './parameters/system-prompt'
 import { TemperatureSelector } from './parameters/temperature'
 import { TopPSelector } from './parameters/top-p'
 
-const defaultSystemPrompt = `You are a helpful AI assistant that remembers previous conversation between yourself the "assistant" and a human the "user":
-### user:
-<previous user message>
-### assistant:
-<previous AI assistant message>
-
-### user:
-<new user prompt>
-
-The AI's task is to understand the context and utilize the previous conversation in addressing the user's questions or requests.`
-
 function Configuration() {
   return (
-    <div className="flex flex-col space-y-4 md:space-y-6">
+    <div className="flex flex-col">
       <div className="flex items-start">
         <div className="space-y-1 pr-2">
           <div className="font-semibold leading-none tracking-tight">
@@ -40,21 +26,8 @@ function Configuration() {
           </div>
         </div>
       </div>
-      <div className="flex flex-1 flex-col space-y-4">
-        <div className="space-y-1.5">
-          <Label className="text-xs">System Prompt</Label>
-          <div className="grid gap-2">
-            <Textarea
-              name="message"
-              className="mb-1 flex min-h-[60px] w-full resize-none rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50"
-              tabIndex={0}
-              defaultValue={defaultSystemPrompt}
-              rows={1}
-              spellCheck={false}
-              readOnly
-            />
-          </div>
-        </div>
+      <div className="mt-6 flex flex-1 flex-col gap-4">
+        <SystemPrompt />
         <TemperatureSelector defaultValue={[0.7]} />
         <MaxTokensSelector defaultValue={[256]} />
         <TopPSelector defaultValue={[0.9]} />
@@ -66,18 +39,6 @@ function Configuration() {
 export function ParametersConfig() {
   return (
     <div className="flex items-center space-x-2">
-      <Drawer.Root>
-        <DrawerTrigger asChild>
-          <Button variant="outline" className="md:hidden">
-            <LucideSettings className="mr-2 h-4 w-4" />
-            Customize
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent className="h-[85%] p-6 pt-10">
-          <Configuration />
-        </DrawerContent>
-      </Drawer.Root>
-
       <div className="hidden md:flex">
         <Popover>
           <PopoverTrigger asChild>
@@ -88,7 +49,7 @@ export function ParametersConfig() {
           </PopoverTrigger>
           <PopoverContent
             align="end"
-            className="z-40 w-[340px] rounded-[0.5rem] bg-white p-6 dark:bg-zinc-950"
+            className="z-40 w-[340px] rounded-[0.5rem] bg-white p-6 px-4 dark:bg-zinc-950"
           >
             <Configuration />
           </PopoverContent>
--- src/components/parameters/top-p.tsx
+++ src/components/parameters/top-p.tsx
@@ -1,12 +1,13 @@
 import { SliderProps } from '@radix-ui/react-slider'
+import { LucideInfo } from 'lucide-react'
 import React from 'react'
 
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '@/components/ui/hover-card'
 import { Label } from '@/components/ui/label'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { Slider } from '@/components/ui/slider'
 
 interface TopPSelectorProps {
@@ -17,41 +18,46 @@ export function TopPSelector({ defaultValue }: TopPSelectorProps) {
   const [value, setValue] = React.useState(defaultValue)
 
   return (
-    <div className="grid gap-2 pt-2">
-      <HoverCard openDelay={200}>
-        <HoverCardTrigger asChild>
-          <div className="grid gap-1.5">
-            <div className="flex items-center justify-between">
-              <Label htmlFor="top-p" className="text-xs">
-                Top P
-              </Label>
-              <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
-                {value}
-              </span>
-            </div>
-            <Slider
-              id="top-p"
-              max={1}
-              defaultValue={value}
-              step={0.1}
-              onValueChange={setValue}
-              className="[&_[role=slider]]:h-4 [&_[role=slider]]:w-4"
-              aria-label="Top P"
-            />
-          </div>
-        </HoverCardTrigger>
-        <HoverCardContent
-          align="start"
-          className="w-[260px] text-sm"
-          side="left"
-        >
-          Dynamically selects the smallest set of tokens whose cumulative
-          probability exceeds the threshold P, and samples the next token only
-          from this set. A float number between `0` and `1`. Set to `1` to
-          disable. Only relevant when `temperature` is set to a value greater
-          than `0`.
-        </HoverCardContent>
-      </HoverCard>
+    <div className="space-y-2">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-1">
+          <Label className="text-xs">Top P</Label>
+          <Popover>
+            <PopoverTrigger>
+              <LucideInfo className="h-3 w-3" />
+              <span className="sr-only">Top P</span>
+            </PopoverTrigger>
+            <PopoverContent
+              className="space-y-3 rounded-[0.5rem] text-sm"
+              side="right"
+              align="start"
+              alignOffset={-20}
+            >
+              <p>
+                Dynamically selects the smallest set of tokens whose cumulative
+                probability exceeds the threshold P, and samples the next token
+                only from this set. A float number between `0` and `1`. Set to
+                `1` to disable. Only relevant when `temperature` is set to a
+                value greater than `0`.
+              </p>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+          {value}
+        </span>
+      </div>
+      <div className="grid gap-4">
+        <Slider
+          id="top-p"
+          max={1}
+          defaultValue={value}
+          step={0.1}
+          onValueChange={setValue}
+          className="[&_[role=slider]]:h-4 [&_[role=slider]]:w-4"
+          aria-label="Top P"
+        />
+      </div>
     </div>
   )
 }
