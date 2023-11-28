b29aa03218f644bac33b0571ff49e9d6d49cdc53
Cyrus Goh <hello@lovincyrus.com>
2023-11-28 15:47:04 -0800

    offline feedback

--- electron/main/managers/transformers.ts
+++ electron/main/managers/transformers.ts
@@ -3,18 +3,19 @@ import { existsSync } from 'node:fs'
 import path from 'node:path'
 
 export class TransformersManager {
-  // TODO: use local model in /models
   modelPath = './.cache'
-  modelName = 'BAAI/bge-large-en-v1.5'
+  modelName = 'Xenova/bge-large-en-v1.5'
 
+  // TODO: omar to add local model initialization
   needsInitialization(modelName: string) {
     return !existsSync(path.join(this.modelPath, modelName))
   }
 
   async embed(fileContent: string) {
-    const { pipeline, env } = await import('@xenova/transformers')
-    const pipe = await pipeline('feature-extraction', this.modelName)
-    env.cacheDir = this.modelPath
+    const { pipeline } = await import('@xenova/transformers')
+    const pipe = await pipeline('feature-extraction', this.modelName, {
+      cache_dir: this.modelPath,
+    })
 
     const embeddings = await pipe(fileContent, {
       pooling: 'mean',
