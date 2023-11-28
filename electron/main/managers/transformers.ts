7a281169ec434214c1ea2adbff2214f79ab3ef17
Cyrus Goh <hello@lovincyrus.com>
2023-11-28 15:21:19 -0800

    embeddings

--- electron/main/managers/transformers.ts
+++ electron/main/managers/transformers.ts
@@ -1,8 +1,27 @@
 import { ipcMain } from 'electron'
+import { existsSync } from 'node:fs'
+import path from 'node:path'
 
 export class TransformersManager {
+  // TODO: use local model in /models
+  modelPath = './.cache'
+  modelName = 'BAAI/bge-large-en-v1.5'
+
+  needsInitialization(modelName: string) {
+    return !existsSync(path.join(this.modelPath, modelName))
+  }
+
   async embed(fileContent: string) {
-    return fileContent
+    const { pipeline, env } = await import('@xenova/transformers')
+    const pipe = await pipeline('feature-extraction', this.modelName)
+    env.cacheDir = this.modelPath
+
+    const embeddings = await pipe(fileContent, {
+      pooling: 'mean',
+      normalize: true,
+    })
+
+    return await embeddings
   }
 
   addClientEventHandlers() {
